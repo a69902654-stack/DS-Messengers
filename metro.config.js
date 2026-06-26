c89f2e9cdbb128ed5fe4d8b013e1481c5b659620
AmirARsalan <a69902654@gmail.com>
2026-06-26 05:25:22 +0330

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -4,17 +4,7 @@ const config = getDefaultConfig(__dirname);
 
 // Fix for @supabase/realtime-js using Node.js 'ws' package
 // which tries to import 'stream' - not available in React Native
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  // Redirect 'stream' to a React Native compatible polyfill
-  if (moduleName === 'stream') {
-    return context.resolveRequest(context, 'readable-stream', platform);
-  }
-  return context.resolveRequest(context, moduleName, platform);
-};
-
-// Force ws to use browser version (not Node.js version)
 config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
   stream: require.resolve('readable-stream'),
 };
 
