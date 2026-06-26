3ec1f758b6c24f472661a945b2d833de315ac604
AmirARsalan <a69902654@gmail.com>
2026-06-26 08:41:27 +0330

    Update supabase.ts

--- config/supabase.ts
+++ config/supabase.ts
@@ -4,7 +4,7 @@ import 'react-native-url-polyfill/auto';
 
 // این مقادیر رو از پنل Supabase بردار
 const supabaseUrl = 'https://ktezdsxfqelvuthxtmks.supabase.co';
-const supabaseAnonKey = 'sb_publishable_GU0wDrKl1ZUvN0zTPKOTrw_8iDHE_lL'; // این رو از Supabase بردار
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0ZXpkc3hmcWVsdnV0aHh0bWtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MDAwODUsImV4cCI6MjA5NTM3NjA4NX0.B5qPoRsKuGBZW1T-YHCsHY4ssRSu-eM-0geUpAZ8Tws'; // این رو از Supabase بردار
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
